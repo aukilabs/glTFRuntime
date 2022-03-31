1b21b79781abecc7a10429fe6f6027d17988446e
Tomas Bartipan <tommy@aukilabs.com>
2022-03-31 11:34:44 +0800

    added precompile flag to build.cs, moved glTF materials to subfolder

--- glTFRuntime.Build.cs
+++ glTFRuntime.Build.cs
@@ -4,63 +4,43 @@ using UnrealBuildTool;
 
 public class glTFRuntime : ModuleRules
 {
-    public glTFRuntime(ReadOnlyTargetRules Target) : base(Target)
-    {
-        PCHUsage = ModuleRules.PCHUsageMode.UseExplicitOrSharedPCHs;
-
-        PublicIncludePaths.AddRange(
-            new string[] {
-				// ... add public include paths required here ...
-			}
-            );
-
-
-        PrivateIncludePaths.AddRange(
-            new string[] {
-				// ... add other private include paths required here ...
-			}
-            );
-
-
-        PublicDependencyModuleNames.AddRange(
-            new string[]
-            {
-                "Core",
+	public glTFRuntime(ReadOnlyTargetRules Target) : base(Target)
+	{
+		PCHUsage = ModuleRules.PCHUsageMode.UseExplicitOrSharedPCHs;
+		
+		PrecompileForTargets = PrecompileTargetsType.Any;
+		
+		PublicDependencyModuleNames.AddRange(
+			new string[]
+			{
+				"Core",
 				// ... add other public dependencies that you statically link with here ...
-                "ProceduralMeshComponent"
-            }
-            );
+				"ProceduralMeshComponent"
+			}
+		);
 
 
-        PrivateDependencyModuleNames.AddRange(
-            new string[]
-            {
-                "CoreUObject",
-                "Engine",
-                "JSON",
-                "RenderCore",
-                "RHI",
-                "ApplicationCore",
-                "Http",
-                "PhysicsCore",
-                "Projects"
+		PrivateDependencyModuleNames.AddRange(
+			new string[]
+			{
+				"CoreUObject",
+				"Engine",
+				"JSON",
+				"RenderCore",
+				"RHI",
+				"ApplicationCore",
+				"Http",
+				"PhysicsCore",
+				"Projects"
 				// ... add private dependencies that you statically link with here ...	
 			}
-            );
+		);
 
-        if (Target.Type == TargetType.Editor)
-        {
-            PrivateDependencyModuleNames.Add("SkeletalMeshUtilitiesCommon");
-            PrivateDependencyModuleNames.Add("UnrealEd");
-            PrivateDependencyModuleNames.Add("AssetRegistry");
-        }
-
-
-        DynamicallyLoadedModuleNames.AddRange(
-            new string[]
-            {
-				// ... add any modules that your module loads dynamically here ...
-			}
-            );
-    }
+		if (Target.Type == TargetType.Editor)
+		{
+			PrivateDependencyModuleNames.Add("SkeletalMeshUtilitiesCommon");
+			PrivateDependencyModuleNames.Add("UnrealEd");
+			PrivateDependencyModuleNames.Add("AssetRegistry");
+		}
+	}
 }
